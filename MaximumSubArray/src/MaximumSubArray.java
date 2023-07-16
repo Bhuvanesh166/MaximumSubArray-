a79a61eaa0116af6512fcd4b1987a3b9020d30b7
Bhuvaneshwaran S <107133289+Bhuvanesh166@users.noreply.github.com>
2023-07-16 14:06:10 +0530

    Add files via upload

---
+++ MaximumSubArray/src/MaximumSubArray.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class MaximumSubArray {
+    public static int findMaximumSubarray(int array[],int size,int max_total)
+    {
+        //find prefix sum
+        int p[]=new int[size];
+        p[0]=array[0];
+        for(int i=1;i<size;i++)
+        {
+            p[i]=p[i-1]+array[i];
+        }
+
+        if(p[size-1]<=max_total)
+        {
+            return p[size-1];
+        }
+        else
+        {
+            for(int i=0;i<size-1;i++)
+            {
+                if(p[size-1]-p[i]<=max_total)
+                {
+                    return p[size-1]-p[i];
+                }
+            }
+        }
+        return 0;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter a size of array");
+        int size=scanner.nextInt();
+        System.out.println("enter max value");
+        int max_total=scanner.nextInt();
+        System.out.println("enter array elements");
+        int array[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            array[i]=scanner.nextInt();
+        }
+        System.out.println(findMaximumSubarray(array,size,max_total));
+
+    }
+
+}
